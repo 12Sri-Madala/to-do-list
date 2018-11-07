bcbbc42d3f0e7ab1fc8399d3af3a109a5299cf9e
Sridhar Madala <Si.madala19@gmail.com>
2018-11-06 17:51:29 -0800

    everything before todo-list part 3

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,16 @@
 import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize';
+import '../assets/css/app.css'
 import React from 'react';
 import List from './list';
+import AddItem from './add_item'
 
 
 const App = () => (
     <div className ="container">
         <h1 className="center">To Do List</h1>
+
+        <AddItem/>
         <List/>
     </div>
 );
